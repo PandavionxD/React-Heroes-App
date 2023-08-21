25d40aa9210d787bac6ee83ef3b158418ced15fc
Alex Daniel <1151372@senati.pe>
2023-08-21 15:35:55 -0500

    Probando imagenes

--- src/components/HeroItem.jsx
+++ src/components/HeroItem.jsx
@@ -4,7 +4,7 @@ export const HeroItem = ({hero}) => {
   return (
     <div className="mx-auto shadow-md rounded-md p-3" > 
         <picture>
-            <img src={`./src/assets/heroes/${hero.id}.jpg`} alt={hero.id} />
+            <img src={`/src/assets/heroes/${hero.id}.jpg`} alt={hero.id} />
         </picture>
         <h2 className="text-center my-2 text-indigo-600 text-lg" >{hero.superhero}</h2>
         <Link to={`/hero/${hero.id}`} className="w-full block text-center outline outline-indigo-600 outline-1 rounded-md text-white bg-indigo-600
