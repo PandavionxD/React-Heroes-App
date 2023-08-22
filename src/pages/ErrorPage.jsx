32446fc189a07c85b5a3ccd31cd48c67fe991648
Alex Daniel <1151372@senati.pe>
2023-08-21 22:23:54 -0500

    Editando Page Error

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,20 @@
+import { Link, useRouteError } from "react-router-dom";
+import { Title } from "../components/Title";
 
 export const ErrorPage = () => {
+  const {status,statusText} = useRouteError();
   return (
-    <h1>ErrorPage</h1>
-  )
-}
+    <div className=" min-h-screen grid place-content-center">
+      <div className="flex flex-col g-3 items-center" >
+        <Title title={status} />
+        <p className="text-center text-lg mb-3" >
+          Page - {statusText}
+        </p>
+        <Link to='/' className="w-full block text-center outline outline-indigo-600 outline-1 rounded-md text-white bg-indigo-600
+        py-1 px-2 text-md hover:text-indigo-600 hover:bg-white transition-all duration-500" >
+        Inicio
+        </Link>
+      </div>
+    </div>
+  );
+};
